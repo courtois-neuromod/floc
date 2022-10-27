94cc625b75d9c5f19d7a448b2304a2f202c6ea5a
test <basile.pinsard@gmail.com>
2022-10-27 08:56:46 -0400

    cleanup

--- src/mk_events/update_events.py
+++ src/mk_events/update_events.py
@@ -48,23 +48,6 @@ def process_log(log_path):
 
 annex2path={}
 
-name_dict = {
-    'def': {
-        'bodies': 'body-xx.jpg',
-        'characters': 'word-xx.jpg',
-        'faces': 'adult-xx.jpg',
-        'objects': 'car-xx.jpg',
-        'places': 'house-xx.jpg'
-    },
-    'alt': {
-        'bodies': 'limb-xx.jpg',
-        'characters': 'word-xx.jpg',
-        'faces': 'adult-xx.jpg',
-        'objects': 'instrument-xx.jpg',
-        'places': 'corridor-xx.jpg'
-    }
-}
-
 cat_dict = {
     'def': {
         'bodies': 'body',
@@ -82,6 +65,7 @@ cat_dict = {
     }
 }
 
+# is only used for a few images which annex key have been changed 
 def get_annex_path(im_path, stim_ds):
     global annex2path
     if '.git/annex' in im_path:
@@ -128,79 +112,54 @@ def update_event(source_list, bids_path, out_path):
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
 
-        #try:
-        log_path = glob.glob(os.path.join(source_path, sub, ses, '*' + fnum + '.log'))
-        assert len(log_path) == 1
-        log_path = log_path[0]
+        try:
+            log_path = glob.glob(os.path.join(source_path, sub, ses, '*' + fnum + '.log'))
+            assert len(log_path) == 1
+            log_path = log_path[0]
 
-        log_dict = process_log(log_path)
-        if not log_dict:
-            continue
-        
-        img_list = log_dict[task]
-        img_list = [get_annex_path(img, stim_ds) for img in img_list]
-        print(img_list)
-
-        df = pd.read_csv(s, sep = '\t')
-        df.insert(loc=4, column='task_version', value=task[-3:], allow_duplicates=True)
-
-        # some ugly code to divide and conquer...
-#            df_stim = df[df['trial_type'] == 'stimuli']
-#            df_nostim = df[df['trial_type'] != 'stimuli']
-
-        df['stim_file'] = df['subcategory'] = df['image_num'] = np.nan
-        df.stim_file.loc[df.trial_type=='stimuli'] = img_list
-        df.subcategory.loc[df.trial_type=='stimuli'] = [get_cat(img, task[-3:], stim_ds) for img in img_list]
-        df.image_num.loc[df.trial_type=='stimuli'] = [get_name(img, task[-3:], stim_ds) for img in img_list]
-
-        
-        """
-        df_stim.insert(loc=1, column='stim_file', value=np.array(img_list), allow_duplicates=True)
-        sub_cat = df_stim.apply(lambda row: get_cat(row, task[-3:], stim_ds), axis=1)
-        df_stim.insert(loc=7, column='subcategory', value=sub_cat, allow_duplicates=True)
-        img_num = df_stim.apply(lambda row: get_name(row, task[-3:], stim_ds), axis=1)
-        df_stim.insert(loc=8, column='image_num', value=img_num, allow_duplicates=True)
-
-        df_nostim.insert(loc=1, column='stim_file', value=np.nan, allow_duplicates=True)
-        df_nostim.insert(loc=7, column='subcategory', value=np.nan, allow_duplicates=True)
-        df_nostim.insert(loc=8, column='image_num', value=np.nan, allow_duplicates=True)
-
-        # probably more elegant ways to merge & re-sort per index...
-        df_merged = pd.concat([df_stim, df_nostim], ignore_index=False)
-        idx = df_merged.index.tolist()
-        df_merged.insert(loc=0, column='temp_idx', value=idx, allow_duplicates=False)
-        df_merged.sort_values(by=['temp_idx'], inplace=True)
-        df_merged = df_merged.drop(['temp_idx'], axis=1)
-        """
-        df_merged = df
-
-        # get run number, to match bold files (run 1 and 2)
-        # (a bit of a hack: onset times only match exactly with itself...)
-        # I tried using first and last trial categories, but there are problems w that approach
-        on_first = df_merged['onset'][0]
-        on_last = df_merged['onset'][df_merged.shape[0]-1]
-        ev_list = glob.glob(os.path.join(bids_path, sub, ses, 'func', '*_task-fLoc_run-*_events.tsv'))
-        assert len(ev_list) == 2
-
-        out_names = []
-        for ev in ev_list:
-            ev_df = pd.read_csv(ev, sep = '\t')
-            if len(ev_df) == 0: #empty file, heudiconv generated, failed run
+            log_dict = process_log(log_path)
+            if not log_dict:
                 continue
-            if ev_df['onset'][0] == on_first and ev_df['onset'][df_merged.shape[0]-1] == on_last:
-                out_names.append(os.path.basename(ev))
-        assert len(out_names) == 1
 
-        out_name = os.path.join(out_dir, out_names[0])
-        df_merged.to_csv(out_name, sep='\t', header=True, index=False)
+            img_list = log_dict[task]
+            img_list = [get_annex_path(img, stim_ds) for img in img_list]
+            print(img_list)
 
-        """
+            df = pd.read_csv(s, sep = '\t')
+            df.insert(loc=4, column='task_version', value=task[-3:], allow_duplicates=True)
+
+
+            df['stim_file'] = df['subcategory'] = df['image_num'] = np.nan
+            df.stim_file.loc[df.trial_type=='stimuli'] = img_list
+            df.subcategory.loc[df.trial_type=='stimuli'] = [get_cat(img, task[-3:], stim_ds) for img in img_list]
+            df.image_num.loc[df.trial_type=='stimuli'] = [get_name(img, task[-3:], stim_ds) for img in img_list]
+
+
+            # get run number, to match bold files (run 1 and 2)
+            # (a bit of a hack: onset times only match exactly with itself...)
+            # I tried using first and last trial categories, but there are problems w that approach
+            on_first = df['onset'][0]
+            on_last = df['onset'][df.shape[0]-1]
+            ev_list = glob.glob(os.path.join(bids_path, sub, ses, 'func', '*_task-fLoc_run-*_events.tsv'))
+            assert len(ev_list) == 2
+
+            out_names = []
+            for ev in ev_list:
+                ev_df = pd.read_csv(ev, sep = '\t')
+                if len(ev_df) == 0: #empty file, heudiconv generated, failed run
+                    continue
+                if ev_df['onset'][0] == on_first and ev_df['onset'][df.shape[0]-1] == on_last:
+                    out_names.append(os.path.basename(ev))
+            assert len(out_names) == 1
+
+            out_name = os.path.join(out_dir, out_names[0])
+            df.to_csv(out_name, sep='\t', header=True, index=False)
+
         except Exception as e:
             print('problems with ' + sub + ', ' + ses)
             print(e)
             import traceback
             print(traceback.format_exc())
-        """
 
 
 if __name__ == '__main__':
